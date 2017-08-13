b0c83797d78042041984c1e9aaebd5bff216a58d
gabyx08 <gaby.ranarz@gmail.com>
2017-08-12 22:47:17 -0500

    responsive

--- js/app.js
+++ js/app.js
@@ -1,41 +1,40 @@
-var cargarPagina = function(){
-      AOS.init();
-      scrollSuave();
-      $('body').scrollspy({ target: '#menu' });
+var cargarPagina = function() {
+    AOS.init();
+    scrollSuave();
+    $('body').scrollspy({ target: '#menu' });
 };
 
-var scrollSuave = function(){
-  $('a[href*="#"]')
+var scrollSuave = function() {
+    $('a[href*="#"]')
 
-    .not('[href="#"]')
-    .not('[href="#0"]')
+        .not('[href="#"]')
+        .not('[href="#0"]')
 
-    .click(function(event) {
-      if (
-        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-        &&
-        location.hostname == this.hostname
-      ) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          event.preventDefault();
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000, function() {
-         
-            var $target = $(target);
-            $target.focus();
-            if ($target.is(":focus")) { 
-              return false;
-            } else {
-              $target.attr('tabindex','-1'); 
-              $target.focus();
-            };
-          });
-        }
-      }
-    });
+        .click(function(event) {
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function() {
+
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) {
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1');
+                            $target.focus();
+                        };
+                    });
+                }
+            }
+        });
 };
 
 $(document).ready(cargarPagina);
